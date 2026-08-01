54b96967b5b8eafddd3dad1f8358fac33eaba910
ContentEchoAI <280157704+ContentEchoAI@users.noreply.github.com>
2026-08-01 20:14:41 +0000

    Revert "Unhide account panel - upgrade button was invisible to all users"
    This reverts commit 1b092d7faf38f246d05705a6522eda18b973153c.

--- app/workspace/page.tsx
+++ app/workspace/page.tsx
@@ -2349,7 +2349,7 @@ function getPlatformDisplayName(value?: string) {
             </p>
           </div>
 
-          <div>{accountPanel}</div>
+          <div className="hidden">{accountPanel}</div>
         </div>
 
         <div className="hidden">
@@ -4040,7 +4040,7 @@ function getPlatformDisplayName(value?: string) {
             </div>
           </div>
 
-          <div>{accountPanel}</div>
+          <div className="hidden">{accountPanel}</div>
 
 
 
